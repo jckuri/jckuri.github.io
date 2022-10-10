4427229d533d21b2ba7821c46877702e329bd96d
jckuri <jckuri@github.com>
2022-10-10 16:20:20 -0500

    Added PC World publications.

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,8 @@
-git add .
-git commit -m "new commit"
-git push -u origin master
+if [ -z "$1" ]
+then
+ echo "You cannot do 'git push' without a proper commit message!"
+else
+ git add .
+ git commit -m "$1"
+ git push -u origin main
+fi
